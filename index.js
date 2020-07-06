2c3dceb011fae14204ac566e6056a7afde1472b8
Andrey Kruglov <kruglov.it@gmail.com>
2020-07-06 20:47:24 +0300

    Relationships

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = `
         name: String
         avatar: String
         postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
     }
     
     enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
         category: PhotoCategory!
         description: String
         postedBy: User!
+        taggedUsers: [User!]!
     }
     
     input PostPhotoInput {
@@ -72,6 +74,13 @@ var photos = [
     }
 ]
 
+var tags = [
+    { "photoID": 1, "userID": "gPlake" },
+    { "photoID": 2, "userID": "sSchmidt" },
+    { "photoID": 2, "userID": "mHattrup" },
+    { "photoID": 2, "userID": "gPlake" },
+]
+
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
@@ -90,11 +99,26 @@ const resolvers = {
             return newPhoto
         }
     },
+
     Photo: {
         url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
+            return users.find(u => u.githubLogin == parent.githubUser)
+        },
+        taggedUsers: parent => tags
+            .filter(tag => tag.photoID === parent.id)
+            .map(tag => tag.userID)
+            .map(userID => users.find(u => u.githubLogin === userID))
+    },
+
+    User: {
+        postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+            .filter(tag => tag.userID === parent.githubLogin)
+            .map(tag => tag.photoID)
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
